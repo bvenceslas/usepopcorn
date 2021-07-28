a18f291699468a69d0ad6fc865961c5ad5f9659b
bvenceslas <bvenceslas@gmail.com>
2021-07-28 12:54:18 +0200

    connect nest app to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 /* eslint-disable */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonsModule } from './persons/persons.module';
 
 @Module({
-  imports: [PersonsModule],
+  // imports: [PersonsModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [PersonsModule, MongooseModule.forRoot('mongodb+srv://testUser:testPass@we-skill-cluster.wkejp.mongodb.net/perso-nest?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
