a1731ebec28d279b07d909def010578096570d46
bvenceslas <bvenceslas@gmail.com>
2021-07-28 12:28:17 +0200

    generate 'persons' resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonsModule } from './persons/persons.module';
 
 @Module({
-  imports: [],
+  imports: [PersonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/persons/persons.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PersonsService } from './persons.service';
+import { PersonsController } from './persons.controller';
+
+@Module({
+  controllers: [PersonsController],
+  providers: [PersonsService]
+})
+export class PersonsModule {}
